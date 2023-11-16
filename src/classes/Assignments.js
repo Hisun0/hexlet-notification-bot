0f3fc83f20707d3aa9541b782372a2d87dc917cc
antoshhkii <velikii.nord@gmail.com>
2023-11-16 12:48:48 +0300

    update: full working class Assignments

--- src/classes/Assignments.js
+++ src/classes/Assignments.js
@@ -1,35 +1,28 @@
-// import Firebase from "./Firebase.js";
-// import config from "../../config.js";
+// import courses from "../../data/courses.js";
 
 class Assignments {
   constructor(coll) {
-    this.coll = coll;
+    this.coll = Object.values(coll);
   }
-
 
   getAvailableAssignments() {
-    return Object.values(this.coll).filter(obj => obj.accessibility === true);
+    this.coll = this.coll.filter(obj => obj.accessibility === true);
+    return this;
   }
-  
-  getAssignmentsByCourseID(courseID) {
-    return Object.values(this.coll).filter(obj => obj.courseID === courseID);
+
+  getCourseAssignments(courseId) {
+    this.coll = this.coll.filter(obj => obj.courseId === courseId);
+    return this;
   }
       
-//  createPromptsForObj(obj) {
-//    return `Доступно задание: (${obj.title})[${obj.url}] до ${obj.due}`;
-//  }
-//
-//  createPrompts() {
-//    return Object.valiues(this.coll).map(obj => Object.values(this).createPromptsForObj(obj)).join('\n\n');
-//  }
+  createPromptsForObj(obj) {
+    return `Доступно задание: (${obj.title})[${obj.url}] до ${obj.due}`;
+  }
+  
+  createPrompts() {
+    const prompts = this.coll.map(obj => this.createPromptsForObj(obj));
+    return prompts.join('\n');
+  }
 };
-
-// const firebase = new Firebase(config);
-// firebase.auth();
-// console.log(config);
-// const coll = await firebase.get('assignments');
-// console.log(coll)
-// const assignments = new Assignments(coll);
-// console.log(assignments.getAvailableAssignments());
 
 export default Assignments;
