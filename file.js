7813058367f116e920928907042111071b91687e
Hisun0 <tridvad1@gmail.com>
2023-11-16 11:20:25 +0300

    delete

--- file.js
+++
@@ -1,9 +0,0 @@
-import Firebase from "./src/classes/Firebase.js";
-import config from "./config.js";
-
-const firebase = new Firebase(config);
-
-firebase.auth();
-
-const coll = await firebase.get(' ', 'assignments');
-console.log(coll);
