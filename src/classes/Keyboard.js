465f26772539600836bc341e4cafb0376b8306e6
Hisun0 <tridvad1@gmail.com>
2023-11-16 11:18:12 +0300

    update: switch from export to default export

--- src/classes/Keyboard.js
+++ src/classes/Keyboard.js
@@ -1,11 +1,13 @@
-export class Keyboard {
+class Keyboard {
   constructor(buttons, options = {}) {
     this.reply_markup = {
-        keyboard: [buttons],
-        resize_keyboard: options.resizeKeyboard || true,
-        one_time_keyboard: options.oneTimeKeyboard || true,
+      keyboard: [buttons],
+      resize_keyboard: options.resizeKeyboard || true,
+      one_time_keyboard: options.oneTimeKeyboard || true,
     };
-    
+
     Object.assign(this, options);
   }
-};
+}
+
+export default Keyboard;
